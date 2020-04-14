c55981f30494e8a1acad848f0eb85c345499ebd0
Rehan <besasengg@gmail.com>
2020-04-14 10:47:31 -0700

    Adding Ajax to cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,7 @@
 class StoreController < ApplicationController
-#class StoreController < ActiveRecord :: Base64
+
+  include CurrentCart
+  before_action :set_cart
   def index
   @products = Product.order(:title)
   end
