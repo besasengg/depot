2a9378c13951b01435fc865b796296953fae7548
Rehan <besasengg@gmail.com>
2020-04-14 06:10:04 -0700

    Adding smart cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,11 +28,13 @@ class LineItemsController < ApplicationController
   def create
     #@line_item = LineItem.new(line_item_params)
 	product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    #@line_item = @cart.line_items.build(product: product)
+	@line_item = @cart.add_product(product)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        #format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+		format.html { redirect_to @line_item.cart}#, notice: 'Line item was successfully created.' }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
@@ -73,6 +75,7 @@ class LineItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart)
+      #params.require(:line_item).permit(:product_id), :cart)
+	  params.require(:line_item).permit(:product_id)
     end
 end
